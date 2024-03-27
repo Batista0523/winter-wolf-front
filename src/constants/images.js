20f77eaab33bad068d2658d5e37387d30290b8fe
elisaul batista <elisaulbatista@pursuit.org>
2024-03-26 20:04:37 -0400

    added new style to home page

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,5 +1,7 @@
 
 import logo from '../assets/logo2.jpg'
+import winterGuys from '../assets/IMG_0669.jpg'
 export default {
+    winterGuys,
     logo
 }
--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
+import images from "../../constants/images";
 
 function Home() {
   return (
     <div>
-    <h1>This is Home Page</h1>
+      <div className="home-section">
+        <section className="img-container">
+          <img className="img-logo" src={images.logo} alt="" />
+          <div className="description-container">
+            <p className="description">
+              Winter Wolf is synonymous with excellence. As leaders in HVAC
+              services, we redefine industry benchmarks with our unwavering
+              focus on quality, reliability, and client satisfaction. Our expert
+              technicians, armed with cutting-edge technology, ensure every
+              project, from installation to maintenance, is executed with
+              precision. Beyond functionality, we prioritize creating
+              environments of comfort, efficiency, and safety for our clients.
+              With prompt responses and tailored solutions, we stand out as the
+              preferred choice for discerning homeowners and businesses. Choose
+              Winter Wolf for the pinnacle of HVAC service—where your
+              satisfaction is not just our priority; it's our promise.
+            </p>
+          </div>
+          
+        </section>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
